beab759c14d94ceda041716bf29214bc1c9e8d7e
Itziar Ormaetxea <itziar.ormaetxea@stay-app.com>
2023-01-12 18:13:36 +0100

    login

--- features/Login/pantallaLogin/login.feature
+++ features/Login/pantallaLogin/login.feature
@@ -2,7 +2,7 @@ Feature: CMS login page
 
 Scenario: an authorized user can access the CMS
   Given I am the hotel staff 
-  #And I access the CMS login page
-  #When I inform correct credentials
-  #And I select to Sign in 
-  #Then I am taken to the hotel dashboard
+  And I access the CMS login page
+  When I inform correct credentials
+  And I select to Sign in 
+  Then I am taken to the hotel dashboard
--- features/steps/pantallaLogin/login.js
+++ features/steps/pantallaLogin/login.js
@@ -4,14 +4,25 @@ const { expect } = require("@playwright/test");
 require('dotenv').config();
 
 Given('I am the hotel staff', async function () {
+   page.env.cms_user="itziar.ormaetxea@stay-app.com";
+});
+
+Given('I access the CMS login page', async function () {
    await page.goto(process.env.cms);
    await page.getByText("Accept all").click();
 });
 
-Given('I access the CMS login page', async function () {
+When('I inform correct credentials', async function () {
    await page.getByPlaceholder("username").click();
    await page.getByPlaceholder("username").fill(process.env.cms_user);
    await page.getByPlaceholder("Password").click();
    await page.getByPlaceholder("Password").fill(process.env.cms_pass);
+ });
+
+ When('I select to Sign in', async function () {
    await page.getByRole("button", { name: "SIGN IN" }).click();
-});
+ });
+
+ Then('I am taken to the hotel dashboard', async function () {
+   await expect(page.getByText("Dashboard")).toBeVisible();
+ });
